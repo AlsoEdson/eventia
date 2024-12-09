e5d74395e1e39ac1b2508c7af6e86106dc535fed
AlsoEdson <edsongutierrez789123@gmail.com>
2024-12-09 17:33:48 -0500

    Change in database.config

--- config/database.config.js
+++ config/database.config.js
@@ -1,11 +1,17 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'DBEventia',
-    password: 'mysql',
-    port: 5432,
+    //connectionString: 'postgres://postgres:mysql@localhost:5432/DBEventia',
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
 });
+
+pool.connect()
+    .then(() => console.log('PostgreSQL connected successfully'))
+    .catch(err => console.error('PostgreSQL connection error: ', err));
 
 module.exports = pool;
